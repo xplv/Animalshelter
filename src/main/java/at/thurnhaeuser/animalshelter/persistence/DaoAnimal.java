129cffd4ba5e95a58ff258f23f8543553b2025ee
ste14288 <ste14288@spengergasse.at>
2015-04-10 22:27:36 +0200

    Cleaned all Mistakes in DaoAnimal

--- src/main/java/at/thurnhaeuser/animalshelter/persistence/DaoAnimal.java
+++ src/main/java/at/thurnhaeuser/animalshelter/persistence/DaoAnimal.java
@@ -1,12 +1,31 @@
 package at.thurnhaeuser.animalshelter.persistence;
 
 import at.thurnhaeuser.animalshelter.model.Animal;
+import at.thurnhaeuser.animalshelter.persistence.Dao;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 /**
  * Created by philip on 10.04.2015.
  */
 public class DaoAnimal implements Dao<Animal> {
     private EntityManager entityManager;
+
+    public DaoAnimal(EntityManager entityManager){this.entityManager=entityManager;}
+
+    @Override
+    public Animal persist(Animal entity) {
+        return null;
+    }
+
+    @Override
+    public Animal findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Animal> findAll() {
+        return null;
+    }
 }
