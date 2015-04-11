7bf32c3dbd26009e1d7b8837c2a71ad588df540f
ste14288 <ste14288@spengergasse.at>
2015-04-11 22:09:16 +0200

    created Interface CompoundRepository and KeeperRepository

---
+++ src/main/java/at/thurnhaeuser/animalshelter/repositories/CompoundRepository.java
@@ -0,0 +1,11 @@
+package at.thurnhaeuser.animalshelter.repositories;
+
+import at.thurnhaeuser.animalshelter.model.Compound;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by philip on 11.04.2015.
+ */
+public interface CompoundRepository extends CrudRepository<Compound, Long> {
+
+}
---
+++ src/main/java/at/thurnhaeuser/animalshelter/repositories/KeeperRepository.java
@@ -0,0 +1,11 @@
+package at.thurnhaeuser.animalshelter.repositories;
+
+import at.thurnhaeuser.animalshelter.model.Keeper;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by philip on 11.04.2015.
+ */
+public interface KeeperRepository extends CrudRepository<Keeper, Long> {
+
+}
