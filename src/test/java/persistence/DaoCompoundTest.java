249b46c42eb0a87f4ecd3e3e91b147e07e5c9e5a
ste14288 <ste14288@spengergasse.at>
2015-04-11 19:42:23 +0200

    DaoCompoundTest Created

---
+++ src/test/java/persistence/DaoCompoundTest.java
@@ -0,0 +1,35 @@
+package persistence;
+
+import at.thurnhaeuser.animalshelter.model.Compound;
+import at.thurnhaeuser.animalshelter.persistence.DaoCompound;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by philip on 11.04.2015.
+ */
+public class DaoCompoundTest extends AbstractDaoTest {
+    private DaoCompound rootDaoCompound;
+
+    @Before
+    public void before(){
+        rootDaoCompound = new DaoCompound(AbstractDaoTest.entityManager);
+    }
+
+    @Test
+    public void persistNewObject(){
+        Compound compound = buildSampleCompound();
+        Assert.assertNull(compound.getId());
+        rootDaoCompound.persist(compound);
+        Assert.assertNotNull(compound.getId());
+    }
+
+    @Test
+    private Compound buildSampleCompound(){
+        Compound compound = new Compound(46.7,150);
+        compound.setSizeInSqm(150.5);
+        compound.setMaxAnimals(23);
+        return compound;
+    }
+}
