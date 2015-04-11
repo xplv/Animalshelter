70e3ce7c6c363b9e0e30526c2d80e9b488bacc5a
Yanik Thurner <thurner@yanik.at>
2015-04-11 22:27:04 +0200

    added repository annotation

--- src/main/java/at/thurnhaeuser/animalshelter/repositories/AnimalRepository.java
+++ src/main/java/at/thurnhaeuser/animalshelter/repositories/AnimalRepository.java
@@ -2,12 +2,14 @@ package at.thurnhaeuser.animalshelter.repositories;
 
 import at.thurnhaeuser.animalshelter.model.Animal;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by yanik on 4/11/15.
  */
+@Repository
 public interface AnimalRepository  extends CrudRepository<Animal, Long>, AnimalRepostitoryCustom {
     List<Animal> findByExternalReference(String externalReference);
 
--- src/main/java/at/thurnhaeuser/animalshelter/repositories/ToyRepository.java
+++ src/main/java/at/thurnhaeuser/animalshelter/repositories/ToyRepository.java
@@ -2,10 +2,12 @@ package at.thurnhaeuser.animalshelter.repositories;
 
 import at.thurnhaeuser.animalshelter.model.Toy;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by yanik on 4/11/15.
  */
+@Repository
 public interface ToyRepository extends CrudRepository<Toy, Long> {
 
 }
