9f905902a2e872471f9b4f1c4099cc5ed17cb906
ste14288 <ste14288@spengergasse.at>
2015-04-13 20:30:39 +0200

    Package restmvc under test created; AbstractRestController created

---
+++ src/test/java/restmvc/AbstractRestController.java
@@ -0,0 +1,13 @@
+package restmvc;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by philip on 13.04.2015.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = )
+public class AbstractRestController {
+}
